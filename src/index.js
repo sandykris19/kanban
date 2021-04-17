04bf5e794f722298cd9fe9dcc8dff960ac8addbf
= <sandykris19@gmail.com>
2021-04-17 09:42:57 +0530

    feat: Added loggin

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom";
 import "./css/main.css";
-import logo from './logo.svg';
+import logo from "./logo.svg";
+import LogRocket from "logrocket";
 
 import { Tasks } from "./tasks";
 
 function App() {
+  useEffect(() => {
+    LogRocket.init("h3zn7w/dev");
+  }, []);
   return (
     <>
       <h1 className="title">Kanban Board for Projects</h1>
@@ -13,7 +17,7 @@ function App() {
         <Tasks />
       </div>
       <div className="footer">
-        <p style={{fontSize: "2rem"}}>This site is made with React.</p>
+        <p style={{ fontSize: "2rem" }}>This site is made with React.</p>
         <img src={logo} className="App-logo" alt="logo" />
       </div>
     </>
